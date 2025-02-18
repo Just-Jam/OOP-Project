15827fdb82556c17b1a1d0e33a3f6a976c470869
Genisa Gabrielle Lee <2402516@sit.singaporetech.edu.sg>
2025-02-18 12:34:36 +0800

    Update EntityManager.java

--- core/src/main/java/io/github/inf1009/manager/EntityManager.java
+++ core/src/main/java/io/github/inf1009/manager/EntityManager.java
@@ -1,4 +1,26 @@
 package io.github.inf1009.manager;
 
+import com.badlogic.gdx.math.Rectangle;
+
 public class EntityManager {
+    protected float x, y;
+    protected float speed;
+    protected Rectangle bounds;
+
+    public EntityManager(float x, float y, float speed, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.bounds = new Rectangle(x, y, width, height);
+    }
+
+    public void move(float dx, float dy, float delta) {
+        this.x += dx * speed * delta;
+        this.y += dy * speed * delta;
+        bounds.setPosition(x, y);
+    }
+
+    public float getX() { return x; }
+    public float getY() { return y; }
+    public Rectangle getBounds() { return bounds; }
 }
