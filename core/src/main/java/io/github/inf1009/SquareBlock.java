04141357ec5d8828f4b6b28512204144153caa3a
Junwe-i <jjunwei.n@gmail.com>
2025-03-25 12:34:59 +0800

    Update SquareBlock.java

--- core/src/main/java/io/github/inf1009/SquareBlock.java
+++ core/src/main/java/io/github/inf1009/SquareBlock.java
@@ -4,9 +4,9 @@ public class SquareBlock extends BlockShape {
 
     public SquareBlock(int gridX, int gridY, int worldWidth, int worldHeight, BlockShape.BlockType type) {
         super(gridX, gridY, worldWidth, worldHeight, type);
-        this.shape = new boolean[][] {
+        setShape(new boolean[][] {
             {true, true},
             {true, true}	
-        };
+        });
     }
 }
