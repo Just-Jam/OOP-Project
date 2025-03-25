9f6855a8ace055edc445b3b154fe7426f0d8370a
Junwe-i <jjunwei.n@gmail.com>
2025-03-25 12:34:42 +0800

    Update LBlock.java

--- core/src/main/java/io/github/inf1009/LBlock.java
+++ core/src/main/java/io/github/inf1009/LBlock.java
@@ -4,10 +4,10 @@ public class LBlock extends BlockShape {
 
     public LBlock(int gridX, int gridY, int worldWidth, int worldHeight, BlockShape.BlockType type) {
         super(gridX, gridY, worldWidth, worldHeight, type);
-        this.shape = new boolean[][] {
+        setShape(new boolean[][] {
             {true, false},
             {true, false},
             {true, true}
-        };
+        });
     }
 }
