0c30e7f47b206c54645c1ce23d138b9b4e54a516
Junwe-i <jjunwei.n@gmail.com>
2025-03-25 22:08:41 +0800

    Update ViewportManager.java

--- core/src/main/java/io/github/inf1009/manager/ViewportManager.java
+++ core/src/main/java/io/github/inf1009/manager/ViewportManager.java
@@ -5,18 +5,15 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class ViewportManager {
-    private ScreenViewport leftViewport, rightViewport;
     private FitViewport fitViewport;
-    private Stage leftStage, rightStage;
+    private int gameWidth, gameHeight, previewWidth;
 
-    public ViewportManager (int width, int height) {
-        leftViewport = new ScreenViewport();
-        leftStage = new Stage(leftViewport);
-
-        rightViewport = new ScreenViewport();
-        rightStage = new Stage(rightViewport);
-
-        fitViewport = new FitViewport(width, height);
+    public ViewportManager (int gameWidth, int gameHeight, int previewWidth) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.previewWidth = previewWidth;
+        // Total width = gameWidth (game area) + previewWidth (preview area)
+        fitViewport = new FitViewport(gameWidth + previewWidth, gameHeight);
     }
 
     public FitViewport getFitViewport() {
@@ -24,23 +21,11 @@ public class ViewportManager {
     }
 
     public void draw() {
-        leftStage.act();
-        rightStage.act();
-
-        leftViewport.apply();
-        leftStage.draw();
-
-        rightViewport.apply();
-        rightStage.draw();
-
-        fitViewport.apply();
+    	fitViewport.apply();
     }
 
     public void resize(int width, int height) {
-        fitViewport.update(width, height, true);
-        leftViewport.update(fitViewport.getLeftGutterWidth(), height, true);
-        leftViewport.setScreenPosition(0, 0);
-        rightViewport.update(fitViewport.getRightGutterWidth(), height, true);
-        rightViewport.setScreenPosition(fitViewport.getRightGutterX(), 0);
+    	fitViewport.update(width, height, true);
     }
 }
+
